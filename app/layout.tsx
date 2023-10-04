752beb509151bcb87d9776e8add8d6de6aaae5f5
Anav Chand <anavkki@gmail.com>
2023-10-04 18:57:50 +1200

    User Context ✅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
+import SupabaseProvider from "@/providers/SupabaseProvider"
+import UserProvider from '@/providers/userProvider'
 import SideBar from '@/components/SideBar'
 import './globals.css'
 
@@ -8,7 +10,7 @@ const figtree = Figtree({ subsets: ['latin'] })
 export const metadata: Metadata = {
     title: 'Spotify',
     description: 'Listen to music!',
-}
+}   
 
 export default function RootLayout({
     children,
@@ -18,9 +20,13 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={figtree.className}>
-                <SideBar>
-                    {children}
-                </SideBar>
+                <SupabaseProvider>
+                    <UserProvider>
+                        <SideBar>
+                            {children}
+                        </SideBar>
+                    </UserProvider>
+                </SupabaseProvider>
             </body>
         </html>
     )
